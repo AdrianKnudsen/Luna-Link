9d9c1f7cdd7df8a9c784dca087392be2ac5793b0
FerhatO-H04 <131246514+FerhatO-H04@users.noreply.github.com>
2023-12-12 14:09:19 +0100

    Created Post Component

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,9 +1,11 @@
 import {CreatePost} from "../Components/Post/CreatePost"
+import { Post } from "../Components/Post/Post"
 
 export function LandingPage(){
     return(
         <>
             <main>
+                
                 <h1>Main</h1>
             </main>
         </>
