aa3b288f9bddfd0d1c7a0d96513fbe4ccecea8fe
Stein-B06 <Steinb06.kodehode@gmail.com>
2023-12-12 12:38:36 +0100

    Made an array of posts

---
+++ src/Components/PostArray/PostsArray.js
@@ -0,0 +1,82 @@
+// postsArray.js
+const postsArray = [
+  {
+    userName: "Alice",
+    category: "General",
+    post: "Just joined this awesome community! Excited to connect with everyone.",
+  },
+  {
+    userName: "Bob",
+    category: "General",
+    post: "Looking for book recommendations. Any favorites you'd like to share?",
+  },
+  {
+    userName: "Charlie",
+    category: "General",
+    post: "Hello world! Happy to be part of this community.",
+  },
+  {
+    userName: "Eva",
+    category: "General",
+    post: "What's your favorite weekend activity? Mine is hiking.",
+  },
+  {
+    userName: "Frank",
+    category: "General",
+    post: "Discussing the latest tech trends. What are your thoughts on AI?",
+  },
+
+  {
+    userName: "Grace",
+    category: "Streaming",
+    post: "Just binge-watched my favorite series. Can't wait for the next season!",
+  },
+  {
+    userName: "Henry",
+    category: "Streaming",
+    post: "Movie night recommendations, please! I'm in the mood for a thriller.",
+  },
+  {
+    userName: "Ivy",
+    category: "Streaming",
+    post: "Any anime lovers here? Looking for new recommendations.",
+  },
+  {
+    userName: "Jack",
+    category: "Streaming",
+    post: "Just discovered a hidden gem on a streaming platform. Must-watch!",
+  },
+  {
+    userName: "Kate",
+    category: "Streaming",
+    post: "Discussing the latest episodes of the hottest show. Spoiler alert!",
+  },
+
+  {
+    userName: "Liam",
+    category: "Games",
+    post: "Gaming night with friends! What's your favorite multiplayer game?",
+  },
+  {
+    userName: "Mia",
+    category: "Games",
+    post: "Just finished an epic RPG. The storyline was mind-blowing.",
+  },
+  {
+    userName: "Noah",
+    category: "Games",
+    post: "Looking for gaming buddies. Who's up for some co-op action?",
+  },
+  {
+    userName: "Olivia",
+    category: "Games",
+    post: "Indie game recommendations, anyone? Let's support small developers!",
+  },
+  {
+    userName: "Parker",
+    category: "Games",
+    post: "Excited for the upcoming game releases. What are you looking forward to?",
+  },
+];
+
+export default postsArray;
