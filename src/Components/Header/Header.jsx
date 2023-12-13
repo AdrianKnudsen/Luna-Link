dc52f16533dc57af1f9e038fe9811e86703a2f9d
AdrianK-B06 <adriank.b06.kodehode@gmail.com>
2023-12-13 12:14:49 +0100

    made navbar and searchbar
    Added navbar component with logo, added searchbar component with search function.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,24 @@
-import styles from "./Header.module.css"
+import styles from "./Header.module.css";
+import SearchBar from "../SearchBar/SearchBar.jsx";
 
-
-export function Header(){
-    return(
-        <>
-            <h1 className={styles.headerTitle}>Header</h1>
-        </>
-    )
+export function Header() {
+  const handleSearch = (searchResults) => {
+    console.log("Search Results:", searchResults);
+  };
+  return (
+    <>
+      <header className={styles.headerBackground}>
+        <nav className={styles.navbar}>
+          <div className={styles.logoContainer}>
+            <img
+              className={styles.logo}
+              src="public/Luna Link Logo.svg"
+              alt="Logo"
+            />
+          </div>
+          <SearchBar onSearch={handleSearch} placeholder="Search here..." />
+        </nav>
+      </header>
+    </>
+  );
 }
