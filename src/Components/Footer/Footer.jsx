32be25df85472d66a1ff8551b51664edcd154457
FerhatO-H04 <131246514+FerhatO-H04@users.noreply.github.com>
2023-12-15 09:46:01 +0100

    designed footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,14 @@ import { Link } from "react-router-dom";
 export function Footer() {
   return (
     <>
-      <h1 className={styles.footerTitle}>Footer</h1>
+     <div className={styles.footerContainer}>
+      <ul className={styles.footerLinks}>
+        <li className={styles.footerLink}><Link to="/ForumMain" >Forums</Link></li>
+        <li className={styles.footerLink}><Link to="/" >Home</Link></li>
+        <li className={styles.footerLink}><Link to="/About" >About</Link></li>
+      </ul>
+      <p className={styles.footerText}>Copyright 2024 Luna Links</p>
+     </div>
     </>
   );
 }
