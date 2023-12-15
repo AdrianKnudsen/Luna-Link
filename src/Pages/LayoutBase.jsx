a0bfc3e53659bd54df657170392f8c9a46510285
AdrianK-B06 <adriank.b06.kodehode@gmail.com>
2023-12-15 18:12:27 +0100

    some css edits

--- src/Pages/LayoutBase.jsx
+++ src/Pages/LayoutBase.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "../components/Header/Header.jsx";
+import { Header } from "../components/Header/Header";
 import { Footer } from "../Components/Footer/Footer";
 
 export function LayoutBase() {
