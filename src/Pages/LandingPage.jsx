12767ac916c2c4f4e79dd78acf13282d199e670c
FerhatO-H04 <131246514+FerhatO-H04@users.noreply.github.com>
2023-12-19 00:10:16 +0100

    Kan srive ny post gjennom CreatePost component

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,13 +1,13 @@
 import { CreatePost } from "../Components/Post/CreatePost";
-import { Posts } from "../Components/Post/Posts";
+
 import styles from "../Pages/LandingPage.module.css";
+
 
 export function LandingPage() {
   return (
     <>
       <main className={styles.main}>
         <CreatePost />
-        <Posts />
       </main>
     </>
   );
