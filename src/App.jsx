85c0f6d441654b6ac1efc58d20d6e006ecdcacfe
AdrianK-B06 <adriank.b06.kodehode@gmail.com>
2024-01-11 18:45:20 +0100

    edit on some functions and css

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,44 @@ import { ContactPage } from "./Pages/ContactPage";
 import LogoutPage from "./Pages/LogoutPage";
 import FakeLoginPage from "./Pages/FakeLoginPage";
 import MainPage from "./Pages/MainPage";
+import { UserProvider } from "./UserContext";
 
 export function App() {
   const [searchTerm, setSearchTerm] = useState("");
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <LayoutBase
-                searchTerm={searchTerm}
-                setSearchTerm={setSearchTerm}
+      <UserProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <LayoutBase
+                  searchTerm={searchTerm}
+                  setSearchTerm={setSearchTerm}
+                />
+              }
+            >
+              <Route index element={<FakeLoginPage />} />
+              <Route index element={<LandingPage />} />
+              <Route path="/Home" element={<LandingPage />} />
+              <Route path="/Loggedin" element={<MainPage />} />
+              <Route path="/About" element={<About />} />
+              <Route path="/Contact" element={<ContactPage />} />
+              <Route path="/Loggedout" element={<LogoutPage />} /> ll
+              <Route
+                path="*"
+                element={
+                  <h1 className="error">
+                    404 <br /> Page Not Found
+                  </h1>
+                }
               />
-            }
-          >
-            <Route index element={<FakeLoginPage />} />
-            <Route index element={<LandingPage />} />
-            <Route path="/Home" element={<LandingPage />} />
-            <Route path="/Loggedin" element={<MainPage />} />
-            <Route path="/About" element={<About />} />
-            <Route path="/Contact" element={<ContactPage />} />
-            <Route path="/Loggedout" element={<LogoutPage />} /> ll
-            <Route path="*" element={<h1>404 Not Found</h1>} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </UserProvider>
     </>
   );
 }
